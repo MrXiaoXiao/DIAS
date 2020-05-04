33ed3ccb88d0c68b2a4b49edf31f36467fc5b365
MrXiaoXiao <779182110@qq.com>
2020-05-04 08:56:02 +0800

    code_cleaning_0504_2

--- dias_main.py
+++ dias_main.py
@@ -15,6 +15,9 @@ def dias_main(args):
     # load configuration file
     cfgs = yaml.load(open(args.config_file), Loader=yaml.BaseLoader)
 
+    # set gpu id
+    os.environ['CUDA_VISIBLE_DEVICES'] = '{}'.format(args.gpu_id)
+
     # check if train or test
     if not (args.run_train or args.run_test):
         print ('Please set one of the options --train | --test')
@@ -51,7 +54,7 @@ if __name__ == '__main__':
     parser.add_argument('--train', dest='run_train', action='store_true', default=False, help='Launch training')
     parser.add_argument('--test', dest='run_test', action='store_true', default=False, help='Launch testing on a list of Ionograms')
     parser.add_argument('--evaluate', dest='run_eval', action='store_true', default=False, help='Launch evaluation on a list of Ionograms')
-    parser.add_argument('--gpuid',dest='gpu_id',type=str, default=False, help='Gpu id')
+    parser.add_argument('--gpuid',dest='gpu_id',type=int, default=0, help='Run On a certain GPU')
 
     args = parser.parse_args()
     
